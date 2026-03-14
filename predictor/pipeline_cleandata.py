23ca982dd0a51e651f3c54c7acc21eb18f6d585f
Mohamed Ayman <mohamedayman2572002@gmail.com>
2026-03-14 20:54:31 +0200

    Refactor clean_data class and restore functionality

--- predictor/pipeline_cleandata.py
+++ predictor/pipeline_cleandata.py
@@ -35,27 +35,3 @@ class clean_data(BaseEstimator, TransformerMixin):
         X["distance"] = np.log1p(X["distance"])
  
         return X
-
-
-
-
-
-
-
-# def clean_data(X):
-#     X = X.copy()
-#     X.columns = X.columns.str.lower().str.strip().str.replace(' ', '_')
-#     drop_cols = [
-#         'user_id','key','pickup_datetime','user_name','driver_name',
-#         'pickup_latitude','pickup_longitude','dropoff_latitude','dropoff_longitude',
-#         'car_condition','weather','traffic_condition'
-#     ]
-
-#     X = X.drop(columns=drop_cols, errors="ignore")
-
-#     X['is_weekend'] = X['weekday'].isin([5,6]).astype(int)
-#     X = X.drop(columns=['weekday'])
-
-#     X['distance'] = np.log1p(X['distance'])
-
-#     return X
